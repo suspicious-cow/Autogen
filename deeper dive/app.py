46c5399be2e74e9c5b6b101151b2e219e7335904
suspicious-cow <zain_naboulsi@msn.com>
2023-10-13 02:08:29 -0500

    minur bug fix

--- deeper dive/app.py
+++ deeper dive/app.py
@@ -31,7 +31,7 @@ user_proxy = autogen.UserProxyAgent(
     is_termination_msg=lambda x: x.get('content','').rstrip().endswith('TERMINATE'),
     code_execution_config={'work_dir':'web', use_docker: False},
     llm_config=llm_config,
-    system_message="""Reply TERMINATE if the task has been solved at rull satisfaction. Otherwise reply CONTINUE or the reason why the task is not solved yet."""   
+    system_message="""Reply TERMINATE if the task has been solved to full satisfaction. Otherwise reply CONTINUE or the reason why the task is not solved yet."""   
 )
 
 task = """
